33bd833e7570ed25a8f1e108bf80be09651ba5b6
iaevange <iaevange@stud.ntnu.no>
2019-11-16 23:10:49 +0100

    testing new method now

--- tests/newsdao.test.js
+++ tests/newsdao.test.js
@@ -26,6 +26,19 @@ afterAll(() => {
   pool.end();
 });
 
+test("Get breaking news from DB", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    data.length).toBeGreaterThanOrEqual(5)
+    done();
+  }
+
+  newsDao.getBreakingNews(callback);
+});
+
+
 test("Get one news from DB", done => {
   function callback(status, data) {
     console.log(
