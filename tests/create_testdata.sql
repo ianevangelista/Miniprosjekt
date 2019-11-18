d42ee258a7c08b248ecbacb5c6f9322892bec4b8
iaevange <iaevange@stud.ntnu.no>
2019-11-18 15:33:47 +0100

    final testing

--- tests/create_testdata.sql
+++ tests/create_testdata.sql
@@ -5,7 +5,7 @@ INSERT INTO kategori(kategori_id, kategori_navn) VALUES
     (4, "Utenriks");
 
 
-INSERT INTO sak (sak_id, overskrift, innhold, tidspunkt, bilde, kategori_id, viktighet) VALUES
+INSERT INTO sak (sak_id, overskrift, ingress, innhold, tidspunkt, bilde, kategori_id, viktighet) VALUES
     (1, 'Test1', "test", "test", "2019-11-11 20:19:47", "bilde", 1, 1),
     (2, 'Test2', "test", "test", "2019-11-11 20:19:47", "bilde", 2, 1),
     (3, 'Test3', "test", "test", "2019-11-11 20:19:47", "bilde", 3, 1),
